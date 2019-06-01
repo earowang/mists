dcc517aca805c66d4b7f32134c2e77ba1456426e
earowang <earo.wang@gmail.com>
2019-06-01 15:26:03 +1000

    set operations for mists_rle_na

--- R/funs-na-rle.R
+++ R/funs-na-rle.R
@@ -13,7 +13,7 @@ na_rle_expand <- function(x) {
 #' @export
 na_rle_table <- function(x) {
   mutate(
-    count(tibble(lengths = na_rle_lengths(x)), lengths),
+    dplyr::count(dplyr::tibble(lengths = na_rle_lengths(x)), lengths),
     nobs = n * lengths
   )
 }
@@ -26,46 +26,26 @@ intersect.mist_rle_na <- function(x, y, ...) {
   semi_join(x_full, y_full, by = "values")
 }
 
+#' @importFrom dplyr union
 #' @export
-na_rle_ggspinogram <- function(x, y = NULL) {
-  na_runs_x <- na_rle_table(x)
-  na_runs_x <- 
-    mutate(
-      na_runs_x, 
-      xlabs = paste(lengths, brackets(n), sep = "\n"),
-      x = .5 * c(cumsum(nobs) + cumsum(dplyr::lag(nobs, default = 0)))
-    )
-  if (is_null(y)) {
-    ggplot(na_runs_x, aes(x = x, y = 1, width = nobs)) +
-      geom_bar(stat = "identity", colour = "white") +
-      scale_x_continuous(
-        labels = na_runs_x$xlabs,
-        breaks = na_runs_x$x,
-        minor_breaks = NULL
-      ) +
-      labs(x = "runs [frequency]", y = "")
-  } else {
-    intersect_xy <- intersect.mist_rle_na(x, y)
-    overlaps_xy <- dplyr::count(intersect_xy, lengths)
-    frac_intersect <- 
-      transmute(
-        inner_join(overlaps_xy, na_runs_x, by = "lengths"),
-        lengths, frac = n.x / nobs, overlap = TRUE
-      )
-    frac_diff <- mutate(frac_intersect, frac = 1 - frac, overlap = FALSE)
-    frac_xy <- dplyr::bind_rows(frac_intersect, frac_diff)
-    na_runs_xy <- mutate(
-      left_join(na_runs_x, frac_xy, by = "lengths"),
-      overlap = ifelse(is.na(frac), FALSE, overlap),
-      frac = ifelse(is.na(frac), 1, frac)
-    )
-    ggplot(na_runs_xy, aes(x = x, y = frac, width = nobs, fill = overlap)) +
-      geom_bar(stat = "identity", colour = "white") +
-      scale_x_continuous(
-        labels = na_runs_x$xlabs,
-        breaks = na_runs_x$x,
-        minor_breaks = NULL
-      ) +
-      labs(x = "runs [frequency]", y = "proportion of overlaps")
-  }
+union.mist_rle_na <- function(x, y, ...) {
+  x_full <- na_rle_expand(x)
+  y_full <- na_rle_expand(y)
+  full_join(x_full, y_full, by = names(x_full))
+}
+
+#' @importFrom dplyr union_all
+#' @export
+union_all.mist_rle_na <- function(x, y, ...) {
+  x_full <- na_rle_expand(x)
+  y_full <- na_rle_expand(y)
+  dplyr::bind_rows(x_full, y_full)
+}
+
+#' @importFrom dplyr setdiff
+#' @export
+setdiff.mist_rle_na <- function(x, y, ...) {
+  x_full <- na_rle_expand(x)
+  y_full <- na_rle_expand(y)
+  anti_join(x_full, y_full, by = "values")
 }
